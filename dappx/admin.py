5e8781c8bd280911d0077b67cd969eaf9a93eaee
vodkamic <strongtreestein@gmail.com>
2021-03-12 08:40:26 +0900

    Initial Commit

---
+++ dappx/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from dappx.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
